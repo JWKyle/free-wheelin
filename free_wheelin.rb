09f7b8dd9b303e6900e7372fa61fc5822c218843
Jim Kyle <jim@jwkyle.com>
2017-05-21 17:20:28 -0500

    Code cleanup

--- free_wheelin.rb
+++ free_wheelin.rb
@@ -55,11 +55,9 @@ db.execute(create_table)
     end
 
     def location_finder(db, time, speed)
-        #Need to have it iterate through the distance column only"
         time = time.to_f
         speed = speed.to_f
         max_distance = (((speed/60.0) * time)/2.0).to_f
-        # time_leg = 
         landmarks = db.execute("SELECT * FROM test WHERE distance<= #{max_distance}")
         landmarks.map do |locals|
             puts "You can ride over to #{locals['location']}. It is #{locals['distance']} miles away."
@@ -70,32 +68,26 @@ db.execute(create_table)
 #### DRIVER CODE ####
 
 puts "Welcome to Free Wheelin'!"
-puts "How much time do you have for a bike ride?"
+puts "How many minutes do you have for a bike ride?"
 time = gets.chomp.to_f
 puts "How intense would you like your ride to be?  (L)eisurely, (M)oderate, (W)orkout, or (C)ustom?"
     intensity = gets.chomp.downcase
-    # val_input = false
-    # until val_input
         if intensity == "l"
             puts "You've chosen a leisurely pace, which is roughly 8mph."
             speed = 8.0
-            # val_input = true
         elsif intensity == "m"
             puts "You've chosen a moderate pace, which is roughly 11mph."
             speed = 11.0
-            # val_input = true
         elsif intensity == "w"
             puts "You've chosen a workout ride, which is roughly 14mph."
             speed = 14.0
-            # val_input = true
         elsif intensity == "c"
             puts    "Please choose your average speed in MPH:"
             speed = gets.chomp.to_f
-            # val_input = true
         else
             puts "I didn't understand what you typed.  Please try again."
         end
-    # end
+
 
 location_finder(db, time, speed)
 
